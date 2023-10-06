0c5e302412d47e8bb5fb6543f2a1e9a9e756a703
janpi <janpi@DESKTOP-DNO5S49>
2023-10-06 08:16:46 +0200

    v3

---
+++ PenApp/PenApp/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿ 
+namespace PenApp.Entities
+{
+    public class Employee : EntityBase
+    {
+        public Employee()
+        {
+            
+        }
+
+        public Employee(string name)
+        {
+            
+        }
+
+        public string? FirstName { get; set; }
+
+        public override string ToString() => $"Id: {Id}, FirstName: {FirstName}";
+    }
+}
--- PenApp/PenApp/Repositories/SqlRepository.cs
+++ PenApp/PenApp/Repositories/SqlRepository.cs
@@ -9,23 +9,35 @@ namespace PenApp.Repositories
     {
         private readonly DbSet<T> _dbSet;
         private readonly DbContext _dbContext;
+<<<<<<< HEAD
        
 
         public SqlRepository(DbContext dbContext )
+=======
+
+        public SqlRepository(DbContext dbContext)
+>>>>>>> 1e6be829fd3e870d584b0928cb1a5971be25b8d0
         {
             _dbContext = dbContext;
             _dbSet = _dbContext.Set<T>();
         }
 
+<<<<<<< HEAD
         public event EventHandler<T>? ItemAdded;
 
+=======
+>>>>>>> 1e6be829fd3e870d584b0928cb1a5971be25b8d0
         public IEnumerable<T> GetAll()
         {
             return _dbSet.OrderBy(item => item.Id).ToList();   
         }
 
 
+<<<<<<< HEAD
         public T? GetById(int id)
+=======
+        public T GetById(int id)
+>>>>>>> 1e6be829fd3e870d584b0928cb1a5971be25b8d0
         {
             return _dbSet.Find(id);
         }
@@ -33,7 +45,10 @@ namespace PenApp.Repositories
         public void Add(T item)
         {
             _dbSet.Add(item);
+<<<<<<< HEAD
             ItemAdded?.Invoke(this, item);
+=======
+>>>>>>> 1e6be829fd3e870d584b0928cb1a5971be25b8d0
         }
 
         public void Remove(T item)
